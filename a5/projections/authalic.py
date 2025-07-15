2d14fafd22cb2e6c0649dab49cc68f239165ef1e
Felix Palmer <felixpalmer@gmail.com>
2025-07-15 11:04:06 +0200

    Tidy

--- a5/projections/authalic.py
+++ a5/projections/authalic.py
@@ -8,31 +8,62 @@ import numpy as np
 from typing import cast
 from ..core.coordinate_systems import Radians
 
+# Authalic conversion coefficients obtained from: https://arxiv.org/pdf/2212.05818
+# See: authalic_constants.py for the derivation of the coefficients
+GEODETIC_TO_AUTHALIC = np.array([
+    -2.2392098386786394e-03,
+    2.1308606513250217e-06,
+    -2.5592576864212742e-09,
+    3.3701965267802837e-12,
+    -4.6675453126112487e-15,
+    6.6749287038481596e-18
+], dtype=np.float64)
+
+AUTHALIC_TO_GEODETIC = np.array([
+    2.2392089963541657e-03,
+    2.8831978048607556e-06,
+    5.0862207399726603e-09,
+    1.0201812377816100e-11,
+    2.1912872306767718e-14,
+    4.9284235482523806e-17
+], dtype=np.float64)
+
+# Adaptation of applyCoefficients from DGGAL project: authalic.ec
+#
+# BSD 3-Clause License
+# 
+# Copyright (c) 2014-2025, Ecere Corporation
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 class AuthalicProjection:
     """
     Authalic projection implementation that converts between geodetic and authalic latitudes.
     """
     
-    # Authalic conversion coefficients obtained from: https://arxiv.org/pdf/2212.05818
-    # See: authalic_constants.py for the derivation of the coefficients
-    GEODETIC_TO_AUTHALIC = np.array([
-        -2.2392098386786394e-03,
-        2.1308606513250217e-06,
-        -2.5592576864212742e-09,
-        3.3701965267802837e-12,
-        -4.6675453126112487e-15,
-        6.6749287038481596e-18
-    ], dtype=np.float64)
-
-    AUTHALIC_TO_GEODETIC = np.array([
-        2.2392089963541657e-03,
-        2.8831978048607556e-06,
-        5.0862207399726603e-09,
-        1.0201812377816100e-11,
-        2.1912872306767718e-14,
-        4.9284235482523806e-17
-    ], dtype=np.float64)
-
     def _apply_coefficients(self, phi: Radians, C: np.ndarray) -> Radians:
         """
         Applies coefficients using Clenshaw summation algorithm (order 6)
@@ -54,7 +85,7 @@ class AuthalicProjection:
         u1 = X * u0 - u1 + C[1]
         u0 = X * u1 - u0 + C[0]
         
-        return phi + 2 * sin_phi * cos_phi * u0
+        return cast(Radians, phi + 2 * sin_phi * cos_phi * u0)
 
     def forward(self, phi: Radians) -> Radians:
         """
@@ -66,7 +97,7 @@ class AuthalicProjection:
         Returns:
             Authalic latitude in radians
         """
-        return cast(Radians, self._apply_coefficients(phi, self.GEODETIC_TO_AUTHALIC))
+        return self._apply_coefficients(phi, GEODETIC_TO_AUTHALIC)
 
     def inverse(self, phi: Radians) -> Radians:
         """
@@ -78,6 +109,6 @@ class AuthalicProjection:
         Returns:
             Geodetic latitude in radians
         """
-        return cast(Radians, self._apply_coefficients(phi, self.AUTHALIC_TO_GEODETIC))
+        return self._apply_coefficients(phi, AUTHALIC_TO_GEODETIC)
 
 __all__ = ['AuthalicProjection'] 
