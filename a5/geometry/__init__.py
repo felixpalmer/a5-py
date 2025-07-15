53bce9f8d0d61a2182fe27cbfbddc96e40daa715
Felix Palmer <felixpalmer@gmail.com>
2025-07-15 14:47:29 +0200

    Fix build and move pentagon class

--- a5/geometry/__init__.py
+++ a5/geometry/__init__.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) A5 contributors
 
+from .pentagon import PentagonShape
 from .spherical_polygon import SphericalPolygonShape, SphericalPolygon
 
-__all__ = ['SphericalPolygonShape', 'SphericalPolygon'] 
+__all__ = ['PentagonShape', 'SphericalPolygonShape', 'SphericalPolygon'] 
