5934724006ae604cc7184ce12aff795e0a2c6358
Felix Palmer <felixpalmer@gmail.com>
2025-07-30 17:04:41 +0200

    Remove unused mat2 module

--- a5/math/__init__.py
+++ a5/math/__init__.py
@@ -5,11 +5,9 @@ A5 Math module - gl-matrix style vector and matrix operations
 from . import vec2
 from . import vec3
 from . import quat
-from . import mat2
 
 __all__ = [
     'vec2',
     'vec3',
-    'quat',
-    'mat2'
+    'quat'
 ]
