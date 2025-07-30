184b13c663ca73d05ea13e4b44cf2a15620d321c
Felix Palmer <felixpalmer@gmail.com>
2025-07-30 17:00:01 +0200

    More tidy

--- a5/core/utils.py
+++ a5/core/utils.py
@@ -24,8 +24,6 @@ class Origin(NamedTuple):
     orientation: List[Orientation]
     first_quintant: int
 
-Contour = List[LonLat]
-
 class A5Cell(TypedDict):
     """
     A5 cell with its position information
@@ -34,30 +32,3 @@ class A5Cell(TypedDict):
     segment: int    # Index (0-4) of triangular segment within pentagonal dodecahedron face  
     S: int         # Position along Hilbert curve within triangular segment
     resolution: int # Resolution of the cell
-
-def triangle_area(v1: vec3, v2: vec3, v3: vec3) -> float:
-    """Calculate the area of a triangle given three 3D vertices."""
-    # Calculate edge vectors
-    edge1 = (v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2])
-    edge2 = (v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2])
-    
-    # Calculate cross product
-    cross_x = edge1[1] * edge2[2] - edge1[2] * edge2[1]
-    cross_y = edge1[2] * edge2[0] - edge1[0] * edge2[2]
-    cross_z = edge1[0] * edge2[1] - edge1[1] * edge2[0]
-    
-    # Calculate magnitude of cross product
-    magnitude = math.sqrt(cross_x * cross_x + cross_y * cross_y + cross_z * cross_z)
-    
-    # Area is half the magnitude of the cross product
-    return 0.5 * magnitude
-
-def pentagon_area(pentagon: List[vec3]) -> float:
-    """Calculate the area of a pentagon by triangulation."""
-    area = 0.0
-    v1 = pentagon[0]
-    for i in range(1, 4):
-        v2 = pentagon[i]
-        v3 = pentagon[i + 1]
-        area += abs(triangle_area(v1, v2, v3))
-    return area
